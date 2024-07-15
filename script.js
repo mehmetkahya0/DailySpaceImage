9f103af3d15e13620c15a3ea86e4f40802c4c4f2
Mehmet Kahya <mehmetkahyakas5@gmail.com>
2024-07-16 02:35:51 +0300

    chore: Update page header, add today's date, improve error handling, and display rate limit info

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const apiKey = "DEMO"; // Replace with your own API key
+const apiKey = "DEMO_KEY"; // Replace with your own API key
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
 async function fetchApod() {
   try {
     const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
     const data = await response.json();
     displayApod(data);
+
+    // Display rate limit information
+    const rateLimitInfo = document.getElementById("rate-limit-info");
+    rateLimitInfo.textContent = `Remaining API requests today: ${response.headers.get(
+      "X-RateLimit-Remaining"
+    )}`;
   } catch (error) {
     console.error("Error fetching the APOD:", error);
-    displayFallbackImage();
+    // Show a default image or message if API request fails
+    const apodImage = document.getElementById("apod-image");
+    apodImage.src = "banner.png";
+
+    // Hide other content or display an error message
+    const title = document.getElementById("title");
+    title.textContent = "NASA APOD";
+    const date = document.getElementById("date");
+    date.textContent = "Date";
+    const description = document.getElementById("description");
+    description.textContent = "Failed to fetch data. Please try again later.";
   }
 }
 
@@ -25,19 +38,6 @@ function displayApod(data) {
   title.textContent = data.title;
   date.textContent = data.date;
   description.textContent = data.explanation;
-}
-
-function displayFallbackImage() {
-  const apodImage = document.getElementById("apod-image");
-  const title = document.getElementById("title");
-  const date = document.getElementById("date");
-  const description = document.getElementById("description");
-
-  apodImage.src = "banner.png"; // Display the fallback image
-  title.textContent = "Image Unavailable";
-  date.textContent = "";
-  description.textContent =
-    "Unable to fetch the Astronomy Picture of the Day. Please try again later.";
 }
 
 // Fetch the APOD when the page loads
