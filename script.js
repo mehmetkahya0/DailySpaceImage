c565b65ae50fdd777c52a1432f7c9d763a4773c4
Mehmet Kahya <mehmetkahyakas5@gmail.com>
2024-07-16 02:45:41 +0300

    chore: Update page header, add today's date, improve error handling, and display rate limit info in script.js

--- script.js
+++ script.js
@@ -4,27 +4,37 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 async function fetchApod() {
   try {
     const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
     const data = await response.json();
+    console.log("Data received:", data); // Log the data received from the API
     displayApod(data);
 
-    // Display rate limit information
+    // Display rate limit information if available
     const rateLimitInfo = document.getElementById("rate-limit-info");
-    rateLimitInfo.textContent = `Remaining API requests today: ${response.headers.get(
-      "X-RateLimit-Remaining"
-    )}`;
+    const rateLimitRemaining = response.headers.get("X-RateLimit-Remaining");
+    if (rateLimitInfo && rateLimitRemaining !== null) {
+      rateLimitInfo.textContent = `Remaining API requests today: ${rateLimitRemaining}`;
+    }
   } catch (error) {
     console.error("Error fetching the APOD:", error);
+
     // Show a default image or message if API request fails
     const apodImage = document.getElementById("apod-image");
-    apodImage.src = "banner.png";
+    apodImage.src = "banner2.png";
 
     // Hide other content or display an error message
     const title = document.getElementById("title");
     title.textContent = "NASA APOD";
+
     const date = document.getElementById("date");
     date.textContent = "Date";
+
     const description = document.getElementById("description");
-    description.textContent = "Failed to fetch data. Please try again later.";
+    description.textContent =
+      "Failed to fetch data. Please try again later.";
   }
 }
 
